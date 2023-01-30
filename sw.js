6109985720c7210cb531d14675fd59ea5072f272
Leandro Rolando <lrolando@paypal.com>
2023-01-30 15:08:45 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-10048d332fde44893659.js"
+    "url": "webpack-runtime-c36f494cd845ebc8c3b2.js"
   },
   {
     "url": "framework-7841de737cb78b1de97b.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c23e0a59797d2a4b888c9e74fb9815f2"
+    "revision": "ec4e4d8724d81d565a73a635182d2a7f"
   },
   {
     "url": "polyfill-5910b445c1712afcdc07.js"
