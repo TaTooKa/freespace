864f3399cf63c76f94553db835582247d799c654
TaTooKa <leandro.rolando@gmail.com>
2023-01-30 12:10:43 -0300

    changed sidebar and added custom logo.

---
+++ cy.br.dys.top.ia/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import cybrdystopialogo from './assets/logo.png';
+
+const Logo = () => (
+  <img src={cybrdystopialogo} alt="cybrdystopia logo" style={{ width: 180 }} />
+);
+
+export default Logo;
