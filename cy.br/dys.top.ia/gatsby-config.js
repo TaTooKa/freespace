69f2d97139bf31cf9ab03f9d710a046043b3c6f4
TaTooKa <leandro.rolando@gmail.com>
2023-01-30 01:15:32 -0300

    changes to siteUrl.

--- cy.br/dys.top.ia/gatsby-config.js
+++ cy.br/dys.top.ia/gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://tatooka.github.io/`,
     pathPrefix: '/cy.br/dys.top.ia',
   },
   plugins: [],
