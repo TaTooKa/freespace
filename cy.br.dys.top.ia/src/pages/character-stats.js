cf6fbd3bb80cebfabfeb2f80d4bf04db3767af8c
Leandro Rolando <lrolando@paypal.com>
2023-01-31 12:04:29 -0300

    add form that saves state to localstorage.

--- cy.br.dys.top.ia/src/pages/character-stats.js
+++ cy.br.dys.top.ia/src/pages/character-stats.js
@@ -1,18 +1,52 @@
-import React, { useState, createContext } from "react"
+import React, { useState, useEffect, Component } from "react"
 import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
 import Seo from '@rocketseat/gatsby-theme-docs/src/components/SEO';
 
-export const CharacterStateContext = createContext(null);
+function CharacterStats() {
+  const [inputs, setInputs] = useState(() => {
+    const savedCharacter = JSON.parse(localStorage.getItem("character"))
+    return savedCharacter || {}
+  })
 
-export default function CharacterStats () {
-  const [character, setCharacter] = useState()
-  console.log(character)
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => saveCharacter(inputs), 500);
+    return () => clearTimeout(timeOutId);
+  }, [inputs]);
+
+  const saveCharacter = () => {
+    console.log(inputs);
+    localStorage.setItem("character", JSON.stringify(inputs))
+  }
 
   return (
     <Layout title="Character Stats">
       <Seo title="Character Stats" />
-      <p>To Do</p>
-      <p>character: {character}</p>
+      <form>
+        <label>Enter your name:
+        <input 
+          type="text" 
+          name="username" 
+          value={inputs.username || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Enter your age:
+          <input 
+            type="number" 
+            name="age" 
+            value={inputs.age || ""} 
+            onChange={handleChange}
+          />
+          </label>
+      </form>
     </Layout>
-  );
+  )
 }
+
+export default CharacterStats
