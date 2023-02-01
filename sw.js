d31e63fcab0ae3b38ebbb0ae0522b0081634943b
TaTooKa <leandro.rolando@gmail.com>
2023-02-01 18:45:29 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-df1defed4f6a3b2d6e2c.js"
+    "url": "webpack-runtime-e4aaff655182a00c9f79.js"
   },
   {
     "url": "framework-7841de737cb78b1de97b.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e61a16f16777b9efb793d5abbdceaeac"
+    "revision": "1cc5ec329a13da1ae031178316748882"
   },
   {
     "url": "polyfill-5910b445c1712afcdc07.js"
