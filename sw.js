6296ad9cd04f9db6292f12ec69d6a93ae13587bb
TaTooKa <leandro.rolando@gmail.com>
2023-02-02 18:43:16 -0300

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "f0e45107-d10b2e8596e93ea34333.js"
   },
   {
-    "url": "app-8d91b193ba09183521fe.js"
+    "url": "app-d820966df7e04c81a320.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a3769932657f0400ede282ccf7445629"
+    "revision": "56d654ae80e9f6c428e240c6e0df28c8"
   },
   {
     "url": "polyfill-5910b445c1712afcdc07.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8d91b193ba09183521fe.js`))) {
+  if (!resources || !(await caches.match(`/app-d820966df7e04c81a320.js`))) {
     return await fetch(event.request)
   }
 
