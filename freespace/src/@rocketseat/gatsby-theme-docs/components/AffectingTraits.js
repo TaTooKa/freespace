519ac237fdd98c94c6d13cf0b52de036619e936f
TaTooKa <leandro.rolando@gmail.com>
2026-02-04 23:23:12 -0300

    move trait vars to character-traits.js

--- freespace/src/@rocketseat/gatsby-theme-docs/components/AffectingTraits.js
+++ freespace/src/@rocketseat/gatsby-theme-docs/components/AffectingTraits.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { promptAffectingTraits, traits } from '/src/pages/character-traits.js';
 
 const AffectingTraits = ({ promptName }) => {
 
@@ -7,91 +8,10 @@ const AffectingTraits = ({ promptName }) => {
   const savedTraitsStr = windowGlobal ? windowGlobal.localStorage.getItem("traits") : "{}"
   const savedTraits = JSON.parse(savedTraitsStr) || {}
 
-  const prompts = {
-    "ActUnderPressure": ["skillAcePilot", "skillColonist", "skillHacker", "skillInfiltrator", "skillMechanic", "skillScientist", "skillScoundrel", "skillSharpshooter", "skillShockTrooper", "gearCombatBot", "gearCombatExosuit", "gearEnergyShield", "gearHoloDisguise", "gearJetpack", "gearRepairTool", "gearPlasmaCutter", "vehicleModuleAfterburner", "vehicleModuleEnhancedSensors"],
-    "SecureAnAdvantage": ["skillAcePilot", "skillAstrogator", "skillCommander", "skillExplorer", "skillHacker", "skillMechanic", "skillPerformer", "skillPirate", "skillScholar", "skillScientist", "skillSharpshooter", "skillSniper", "gearRepairTool", "gearPlasmaCutter", "gearReconDrone", "gearSmartGun", "vehicleModuleAfterburner", "vehicleModuleGrappler", "vehicleModuleMissileArray", "vehicleModuleShields", "vehicleModuleStealthTech"],
-    "LeadTheTeam": ["skillCommander", "skillSpaceMarine"],
-    "GatherInformation": ["skillAstrogator", "skillBountyHunter", "skillColonist", "skillDiplomat", "skillExplorer", "skillInfiltrator", "skillJournalist", "skillLawEnforcement", "skillScholar", "gearPersonalAssistant", "gearProtocolBot", "vehicleModuleEnhancedSensors", "vehicleModuleResearchLab"],
-    "Compel": ["skillBrawler", "skillDiplomat", "skillFreeTrader", "skillJournalist", "skillLawEnforcement", "skillPerformer", "gearHoloDisguise", "gearProtocolBot", "vehicleModuleAestheticCustomizations"],
-    "UseYourGear": ["skillScavenger", "vehicleModuleWorkshop"],
-    "Clash": ["skillBrawler", "skillPirate", "skillScoundrel", "skillSharpshooter", "skillSniper", "skillSpaceMarine", "gearCombatBot", "gearCombatExosuit", "gearHeavyWeapon", "gearJetpack", "gearSmartGun", "vehicleModuleGrappler", "vehicleModuleMissileArray", "vehicleModuleWeaponryOvercharge"],
-    "AcceptAChallenge": ["skillBountyHunter", "skillShockTrooper"],
-    "MakeProgress": ["vehicleModuleMissileArray"],
-    "FulfillAChallenge": ["skillAcePilot", "skillBountyHunter", "skillBrawler", "skillColonist", "skillCommander", "skillDiplomat", "skillExplorer", "skillFreeTrader", "skillHacker", "skillInfiltrator", "skillJournalist", "skillLawEnforcement", "skillScoundrel", "skillSharpshooter", "skillSpaceMarine"],
-    "FailAChallenge": [],
-    "EndureHarm": ["gearEnergyShield"],
-    "EndureStress": [],
-    "SacrificeResources": ["skillScavenger", "gearEnergyShield", "vehicleModuleMissileArray"],
-    "FaceDeath": [],
-    "FaceDesolation": [],
-    "Heal": ["skillMedic", "gearNanoMedKit", "vehicleModuleMedbay"],
-    "Relax": ["skillJournalist", "skillPerformer", "vehicleModuleRelaxingArea"],
-    "Resupply": ["skillFreeTrader", "skillMedic", "skillScavenger", "gearReconDrone", "vehicleModuleExpandedHold", "vehicleModuleWorkshop"],
-    "PayTheConsequences": ["vehicleModuleReinforcedHull", "vehicleModuleShields"],
-    "QueryTheFates": [],
-    "ImproveYourself": [],
-    "Retire": [],
-  }
-
-  const traits = {
-    "skillAcePilot": "ACE PILOT",
-    "skillAstrogator": "ASTROGATOR",
-    "skillBountyHunter": "BOUNTY HUNTER",
-    "skillBrawler": "BRAWLER",
-    "skillColonist": "COLONIST",
-    "skillCommander": "COMMANDER",
-    "skillDiplomat": "DIPLOMAT",
-    "skillExplorer": "EXPLORER",
-    "skillFreeTrader": "FREE TRADER",
-    "skillHacker": "HACKER",
-    "skillInfiltrator": "INFILTRATOR",
-    "skillJournalist": "JOURNALIST",
-    "skillLawEnforcement": "LAW ENFORCEMENT",
-    "skillMechanic": "MECHANIC",
-    "skillMedic": "MEDIC",
-    "skillPerformer": "PERFORMER",
-    "skillPirate": "PIRATE",
-    "skillScavenger": "SCAVENGER",
-    "skillScholar": "SCHOLAR",
-    "skillScientist": "SCIENTIST",
-    "skillScoundrel": "SCOUNDREL",
-    "skillSharpshooter": "SHARPSHOOTER",
-    "skillShockTrooper": "SHOCK TROOPER",
-    "skillSniper": "SNIPER",
-    "skillSpaceMarine": "SPACE MARINE",
-    "gearCombatBot": "COMBAT BOT",
-    "gearCombatExosuit": "COMBAT EXOSUIT",
-    "gearEnergyShield": "ENERGY SHIELD",
-    "gearHeavyWeapon": "HEAVY WEAPON",
-    "gearHoloDisguise": "HOLO-DISGUISE",
-    "gearJetpack": "JETPACK",
-    "gearRepairTool": "REPAIR TOOL",
-    "gearNanoMedKit": "NANO-MEDKIT",
-    "gearPersonalAssistant": "PERSONAL ASSISTANT",
-    "gearPlasmaCutter": "PLASMA CUTTER",
-    "gearProtocolBot": "PROTOCOL BOT",
-    "gearReconDrone": "RECON DRONE",
-    "gearSmartGun": "SMART GUN",
-    "vehicleModuleAestheticCustomizations": "AESTHETIC CUSTOMIZATIONS",
-    "vehicleModuleAfterburner": "AFTERBURNER",
-    "vehicleModuleEnhancedSensors": "ENHANCED SENSORS",
-    "vehicleModuleExpandedHold": "EXPANDED HOLD",
-    "vehicleModuleGrappler": "GRAPPLER",
-    "vehicleModuleMedbay": "MEDBAY",
-    "vehicleModuleMissileArray": "MISSILE ARRAY",
-    "vehicleModuleReinforcedHull": "REINFORCED HULL",
-    "vehicleModuleRelaxingArea": "RELAXING AREA",
-    "vehicleModuleResearchLab": "RESEARCH LAB",
-    "vehicleModuleShields": "SHIELDS",
-    "vehicleModuleStealthTech": "STEALTH TECH",
-    "vehicleModuleWeaponryOvercharge": "WEAPONRY OVERCHARGE",
-    "vehicleModuleWorkshop": "WORKSHOP",
-  }
-
   var affectingTraits = {};
 
-  if (prompts[promptName] !== undefined) {
-    prompts[promptName].forEach(traitKey => {
+  if (promptAffectingTraits[promptName] !== undefined) {
+    promptAffectingTraits[promptName].forEach(traitKey => {
       if (savedTraits[traitKey] !== undefined && savedTraits[traitKey] == true) {
         affectingTraits[traitKey] = traits[traitKey];
       }
